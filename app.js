9b289aa542501a0ca94fda90f8aaf8c3bbd3815b
NIKHIL MISHRA <30726256+nikhil312@users.noreply.github.com>
2020-05-18 11:28:21 +0530

    Boult

---
+++ app.js
@@ -0,0 +1,14 @@
+// code written by @webdev_guru
+document.getElementById("send__btn").addEventListener("click", function (e) {
+  var userValue = document.getElementById("user_value").value;
+  var userMessage = document.getElementById("user_message").value;
+
+  var send_message =
+    "https://api.whatsapp.com/send?phone=+91" +
+    userValue +
+    "&text=" +
+    uesrMessage;
+  window.open(send_message);
+  e.preventDefault();
+  return false;
+});
